4a19f617aad00546e5179ce3dd0fe579ac81e0c1
rahulqed <rahul.deshmukh324.rdr@gmail.com>
2020-10-23 19:32:35 +0530

    Static example with the static blocks/function and main in diffrent classes

--- src/com/javapractice/StaticExample/StaticExample.java
+++ src/com/javapractice/StaticExample/StaticExample.java
@@ -1,20 +1,23 @@
 package com.javapractice.StaticExample;
 
-class StaticExample {
+class StaticClass {
 
-     static void staticfun()
-    {
+    static void staticfun() {
         System.out.println("Control is inside staticfun");
     }
+
     static {
         System.out.println("Inside 1st static block");
     }
+
     static {
         System.out.println("Inside 2nd static block");
     }
 
+}
+ class StaticExampple {
     public static void main(String args[])  {
-        staticfun();
+        StaticClass.staticfun();
     }
 }
-
+//In case the main method and the static function/block are in the same class we do  not need anything to call the funtion;just the name of the funtion is enough
